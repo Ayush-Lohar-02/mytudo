28e97bf763a798d338e20646e295c0b86103fd4e
AyushLohar <aryanmlv@gmail.com>
2024-05-30 13:21:45 +0530

    Adding more funcitonality

--- src/todo.js
+++ src/todo.js
@@ -5,6 +5,9 @@ import "./Todo.css";
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState("");
+  const [editIndex, setEditIndex] = useState(null);
+  const [editText, setEditText] = useState("");
+  const [showTodos, setShowTodos] = useState(true);
 
   const addTodo = () => {
     if (newTodo.trim() !== "") {
@@ -18,43 +21,89 @@ const TodoList = () => {
     setTodos(updatedTodos);
   };
 
+  const editTodo = (index) => {
+    setEditIndex(index);
+    setEditText(todos[index]);
+  };
+
+  const saveTodo = () => {
+    const updatedTodos = todos.map((todo, index) =>
+      index === editIndex ? editText : todo
+    );
+    setTodos(updatedTodos);
+    setEditIndex(null);
+    setEditText("");
+  };
+
+  const toggleDisplay = () => {
+    setShowTodos(!showTodos);
+  };
+
   return (
     <div className="container">
       <h1>Todo List</h1>
       <Form>
-        <Form.Group className="Form-group">
+        <Form.Group className="form-inline">
           <Form.Control
             type="text"
             placeholder="Enter a new todo"
             value={newTodo}
             onChange={(e) => setNewTodo(e.target.value)}
-            className=" textModiefy"
+            className="textModiefy"
           />
-          <Button
-            variant="primary"
-            onClick={addTodo}
-            className="Button addtodo"
-          >
+          <Button variant="primary" onClick={addTodo} className="addtodo  ">
             Add
+          </Button>
+          <Button variant="secondary" onClick={toggleDisplay} className="ml-2 show-todos">
+            {showTodos ? "Hide" : "Display"}
           </Button>
         </Form.Group>
       </Form>
-      <ListGroup className="list-group mt-3">
-        {todos.map((todo, index) => (
-          <ListGroup.Item key={index} className="list-group-item">
-            {todo}
-            <div className="remove-button-container">
-              <Button
-                variant="danger"
-                className="float-right remove"
-                onClick={() => removeTodo(index)}
-              >
-                Remove
-              </Button>
-            </div>
-          </ListGroup.Item>
-        ))}
-      </ListGroup>
+      {showTodos && (
+        <ListGroup className="list-group mt-3">
+          {todos.map((todo, index) => (
+            <ListGroup.Item key={index} className="list-group-item">
+              {editIndex === index ? (
+                <>
+                  <Form.Control
+                    type="text"
+                    value={editText}
+                    onChange={(e) => setEditText(e.target.value)}
+                    className="editModiefy"
+                  />
+                  <Button
+                    variant="success"
+                    onClick={saveTodo}
+                    className="ml-2 save"
+                  >
+                    Save
+                  </Button>
+                </>
+              ) : (
+                <>
+                  {todo}
+                  <div className="button-container">
+                    <Button
+                      variant="warning"
+                      className="float-right edit"
+                      onClick={() => editTodo(index)}
+                    >
+                      Edit
+                    </Button>
+                    <Button
+                      variant="danger"
+                      className="float-right remove"
+                      onClick={() => removeTodo(index)}
+                    >
+                      Remove
+                    </Button>
+                  </div>
+                </>
+              )}
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      )}
     </div>
   );
 };
